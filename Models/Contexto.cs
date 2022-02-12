39543e33ee789389e4e391cb7c9a39964f851da9
eliferretti <eliferretti14@gmail.com>
2022-02-12 19:57:40 -0300

    Update Contexto.cs

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -19,8 +19,10 @@ namespace Andor.Models
             // optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-VV6TL78;Database=DBANDOR_TTTT;Integrated Security=True");
 
             // conexao azure
-            optionsBuilder.UseSqlServer(connectionString: @"Data Source = andorserver.database.windows.net; Initial Catalog = BETE_DBANDOR; User ID = squad46; Password =#RecodePro;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-           
+            // optionsBuilder.UseSqlServer(connectionString: @"Data Source = andorserver.database.windows.net; Initial Catalog = BETE_DBANDOR; User ID = squad46; Password =#RecodePro;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            
+            // conexao somee.com
+            optionsBuilder.UseSqlServer(connectionString: @"workstation id = DBANDOR.mssql.somee.com; packet size = 4096; user id = Ferretti_SQLLogin_1; pwd = khoi5d5m8w; data source = DBANDOR.mssql.somee.com; persist security info = False; initial catalog = DBANDOR");
         }
     }        
 }
